87322135fbc6209948f0535f4058f48c37033241
joshsingh <joshsingh@joshsinghs-MacBook-Air.local>
2024-12-28 18:07:05 +0900

    dsjkddkkkk

--- carbon_app.py
+++ carbon_app.py
@@ -70,7 +70,7 @@ def add_logo():
         div[data-baseweb="slider"] div[class*="InnerThumb"],
         div[data-baseweb="slider"] div[class*="ThumbValue"] {
             background-color: #fafcff!important;
-            color: #2c3e50 !important;
+            color: #4abdbe !important;
         }
 
         /* Remove the turquoise background */
